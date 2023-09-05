4d40a57efeaa468b1621ef524841ee5c76a2276a
umesh_pati1 <umesh.patil@moonraft.com>
2023-09-05 16:38:15 +0530

    handled all edge cases

--- index.js
+++ index.js
@@ -3,40 +3,179 @@
 // minimum slides 4 , not working
 
 const container = document.querySelector(".container");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+const maskEl = document.querySelector(".mask");
 
 const galleryEL = container.querySelector(".gallery");
 const slides = galleryEL.querySelectorAll(".slide");
 
-const transitionTime = 3000;
-const slidesLength = slides.length;
+const dots = document.querySelectorAll(".dot");
+var interval;
 
+dots.forEach((dot) => {
+  dot.addEventListener("click", dotClick);
+});
+
+const transitionTime = 2000;
+const slidesLength = slides.length;
+const autoplay = true;
+
+var mylatesttap;
+
+console.assert(slides.length === dots.length, "length mismatch");
+let currentIndex = 0;
 let rotate = 0;
-let currentIndex = 2;
+
+dots[currentIndex].classList.add("active");
 
 container.style.setProperty("--t", `${transitionTime}ms`);
 
-setInterval(() => {
-  hide = (currentIndex - 2 + slidesLength) % slidesLength;
-  show = (currentIndex + 4) % slidesLength;
+function next() {
+  clearInterval(interval);
+  console.log("hi");
+  const prev = (currentIndex + 2) % slidesLength;
+  const next = (currentIndex - 2 + slidesLength) % slidesLength;
+  const dummy = (currentIndex + 3) % slidesLength;
 
-  const hideEl = slides[hide];
-  const showEl = slides[show];
-  const styles = getComputedStyle(hideEl);
-  const newRotate = styles.getPropertyValue("rotate");
+  const dummyEl = slides[dummy];
+  const prevEl = slides[prev];
+  const nextEl = slides[next];
 
-  if (slides.length >= 6) {
-    hideEl.style.display = "none";
-    showEl.style.display = "block";
-    showEl.style.rotate = newRotate;
-  } else if (slides.length === 4) {
-    hideEl.style.rotate = parseInt(newRotate) - 120 + "deg";
-  } else if (slides.length === 5) {
-    hideEl.style.rotate = parseInt(newRotate) - 60 + "deg";
-  }
+  const prevElStyles = getComputedStyle(prevEl);
+  const newRotate = prevElStyles.getPropertyValue("rotate");
+
+  dummyEl.style.display = "block";
+
+  dummyEl.style.rotate = parseInt(newRotate) + 60 + "deg";
+
+  nextEl.style.display = "none";
 
   currentIndex = (currentIndex + 1) % slidesLength;
-  rotate = rotate + 60;
-  galleryEL.style.transform = `rotate(-${rotate}deg)`;
 
-  // if your gallery is in view then only start the animation , otherwise reset to initial state
-}, transitionTime);
+  const activeDot = document.querySelector(".dot.active");
+
+  activeDot.classList.remove("active");
+
+  // currentIndex = index;
+  dots[currentIndex].classList.add("active");
+
+  rotate = rotate - 60;
+  galleryEL.style.transform = `rotate(${rotate}deg)`;
+
+  autoplayFn();
+}
+
+function prev() {
+  clearInterval(interval);
+
+  const prev = (currentIndex - 2 + slidesLength) % slidesLength;
+  const next = (currentIndex + 2) % slidesLength;
+  const dummy = (currentIndex - 3 + slidesLength) % slidesLength;
+
+  const dummyEl = slides[dummy];
+  const prevEl = slides[prev];
+  const nextEl = slides[next];
+
+  const prevElStyles = getComputedStyle(prevEl);
+  const newRotate = prevElStyles.getPropertyValue("rotate");
+
+  dummyEl.style.display = "block";
+
+  dummyEl.style.rotate = parseInt(newRotate) - 60 + "deg";
+
+  nextEl.style.display = "none";
+
+  currentIndex = (currentIndex - 1 + slidesLength) % slidesLength;
+
+  const activeDot = document.querySelector(".dot.active");
+
+  activeDot.classList.remove("active");
+
+  // currentIndex = index;
+  dots[currentIndex].classList.add("active");
+
+  rotate = rotate + 60;
+
+  // galleryEL.style. = `${rotate}deg`;
+  galleryEL.style.transform = `rotate(${rotate}deg)`;
+
+  console.log("gallery", galleryEL);
+
+  autoplayFn();
+}
+
+console.log(nextBtn);
+prevBtn.addEventListener("click", () => {
+  tap(prev);
+});
+nextBtn.addEventListener("click", () => {
+  tap(next);
+});
+
+function autoplayFn() {
+  if (autoplay == true)
+    interval = setInterval(() => {
+      next();
+    }, transitionTime);
+}
+
+autoplayFn();
+function tap(fn) {
+  var now = new Date().getTime();
+  var timesince = now - mylatesttap;
+
+  if (isNaN(timesince) || timesince > transitionTime) {
+    fn();
+    mylatesttap = new Date().getTime();
+  } else {
+    console.log("clicked quikly");
+  }
+}
+
+function dotClick(event) {
+  const activeDot = document.querySelector(".dot.active");
+
+  const index = Array.from(dots).indexOf(event.target);
+
+  const prevIndex = Array.from(dots).indexOf(activeDot);
+
+  activeDot.classList.remove("active");
+
+  // currentIndex = index;
+  dots[index].classList.add("active");
+
+  if (index > prevIndex) {
+    let count = index - prevIndex;
+
+    if (count > 2) {
+      container.style.setProperty("--t", `${100}ms`);
+    }
+    for (let i = 0; i < count; i++) {
+      next();
+    }
+  } else {
+    let count = prevIndex - index;
+
+    if (count > 2) {
+      container.style.setProperty("--t", `${100}ms`);
+    }
+    for (let i = 0; i < count; i++) {
+      prev();
+    }
+  }
+  setTimeout(() => {
+    container.style.setProperty("--t", `${transitionTime}ms`);
+  }, 50);
+}
+
+maskEl.addEventListener("click", (e) => {
+  const { top, left, width, height } = e.target.getBoundingClientRect();
+
+  const x = e.clientX - left; //x position within the element.
+  // const y = e.clientY - top; //y position within the element.
+
+  if (x < width / 4) prev();
+  else if (x > (3 * width) / 4) next();
+});
